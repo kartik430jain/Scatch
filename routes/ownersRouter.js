1ad00b5b631e518acaa26aabd0c4fae0a37f3d05
kartik430jain <140139863+kartik430jain@users.noreply.github.com>
2025-02-01 20:26:06 +0530

    Update ownersRouter.js

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -6,13 +6,28 @@ router.get("/",function(req,res){
     res.send("hey it's Working");
 });
 
-//  console.log(process.env.NODE_ENV);
-
-
 if(process.env.NODE_ENV === "development"){
 
-    router.post("/create",function(req,res){
-        res.send("hey it's Working");
+    router.post("/create", async function(req,res){
+        let owners = await ownerModel.find();
+        if(owners.length > 0 ) {
+            return res
+                .status(502)
+                .send("You Dont have permisiion to create a new owner");
+        }
+
+        let {fullname, email, password} = req.body;
+
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+            
+        });
+
+        res.status(201).send(createdOwner);
+            
+        
     });
 }
 
